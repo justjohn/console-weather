43f34095c34b1d7db09c4a2aa2604d87966cab05
John Roepke <john@justjohn.us>
2012-10-18 00:20:20 -0400

    structural updates, better formatting

---
+++ src/api.js
@@ -0,0 +1,79 @@
+var http = require('http'),
+	Q = require('q');
+
+var API = function(api_key) {
+	this.api_key = api_key;
+};
+
+API.prototype.call = function(path) {
+	var deferred = Q.defer();
+
+	var options = {
+	  host: 'api.wunderground.com',
+	  port: 80,
+	  path: '/api/' + this.api_key + path,
+	  method: 'GET'
+	};
+
+	var req = http.request(options, function(res) {
+		var output = '';
+		res.setEncoding('utf8');
+		res.on('data', function (chunk) {
+			output += chunk;
+		});
+
+		res.on('end', function (chunk) {
+			var json = JSON.parse(output);
+			deferred.resolve(json);
+		});
+	});
+
+	req.on('error', function(e) {
+	  	deferred.reject('problem with request: ' + e.message);
+	});
+
+	req.end();
+
+	return deferred.promise;
+};
+
+API.prototype.geolocate = function() {
+	var deferred = Q.defer();
+
+	this.call('/geolookup/q/autoip.json').then(function(data) {
+		if (!data.location) {
+			var error = data.response.error;
+			deferred.reject(error);
+
+		} else {
+			deferred.resolve(data.location);
+		}
+
+	}, function(err) {
+		deferred.reject({
+			type: 'callfail',
+			description: err
+		});
+	});
+
+	return deferred.promise;
+};
+
+API.prototype.forecast = function(zip) {
+	var deferred = Q.defer();
+
+	this.call('/conditions/forecast/q/' + zip + '.json').then(function(data) {
+		deferred.resolve(data);
+
+	}, function(err) {
+		deferred.reject({
+			type: 'callfail',
+			description: err
+		});
+	});
+
+	return deferred.promise;
+};
+
+exports.API = API;
+
---
+++ src/utils.js
@@ -0,0 +1,46 @@
+exports.repeat = function(str, length) {
+	return new Array(length + 1).join(str);
+};
+
+exports.padLeft = function(string, pad, length) {
+	return (exports.repeat(pad, length) + string).slice(-length);
+};
+
+exports.padRight = function(string, pad, length) {
+	return (string + exports.repeat(pad, length)).slice(0, length);
+};
+
+exports.pad = function(string, length) {
+	var pad = (length - string.length) / 2,
+		intPad = Math.floor(pad),
+		start = intPad + (pad == intPad?0:1);
+
+	return exports.repeat(" ", start) + string + exports.repeat(" ", intPad);
+}
+
+exports.wordAwareFormat = function(string, length) {
+	var words = string.split(" ");
+
+	var output = "",
+		line_length = 0,
+		line = [];
+
+	while(words.length > 0) {
+		var word = words.shift(),
+			word_length = word.length + 1;
+
+		line_length += word_length;
+		if (line_length > length) {
+			output += line.join(" ") + "\n";
+			line = [];
+			line_length = word_length;
+		}
+
+		line.push(word);
+	}
+
+	if (line.length > 0)
+		output += line.join(" ") + "\n";
+
+	return output;
+};
